3759d5a7913791f3fc2097fbb3a98d6adbd7e86b
Hariharan1024 <hariharan.nexevo@gmail.com>
2022-02-09 12:37:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {HashRouter , Routes, Route} from 'react-router-dom';
+import Home from './components/home';
+
+
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      </Routes>
+    </HashRouter>
+  );
+}
+
+export default App;
